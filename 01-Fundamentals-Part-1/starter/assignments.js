4efa37823898dd1982f516a5fd65831d16b2c35b
Vasso Vassiliades <vaso@vaseo.co.za>
2022-06-29 21:18:04 +0200

    added isisland

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,9 @@
-let country = 'South Africa';
-let continent = 'Africa'
-let population = 5 + ' mil'
+const country = 'South Africa';
+const continent = 'Africa'
+let population = 50 + ' mil'
 
 console.log(country, continent, population)
+
+const isIsland = false;
+const language = 'english';
+console.log(country, continent, population, isIsland, language)
