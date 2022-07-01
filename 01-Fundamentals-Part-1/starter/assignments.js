ca48d6bde8cf7ed898cd61b20a3a6bba3b5b20e2
Vasso Vassiliades <vaso@vaseo.co.za>
2022-07-01 09:03:17 +0200

    Tested typeof for Data Variables

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,8 +1,8 @@
 const country = 'South Africa';
-const continent = 'Africa'
-let population = 50 + ' mil'
+const continent = 'Africa';
+let population = 50 + ' mil';
 
-console.log(country, continent, population)
+console.log(country, continent, population);
 
 const isIsland = false;
 const language = 'english';
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,34 @@
+/*
 let js = 'amazing';
-if (js === 'amazing') alert("JS is FUN!");
+if (js === 'amazing') console.log("JS is FUN!");
 
 let firstName = 'Vassili Peter';
 console.log(firstName);
-alert(firstName);
+
+
+// Variable name conventions
+let vassili_peter = "VP";
+let $function = 27;
+
+let person = "Vassili";
+let PI = 3.1415;
+
+let myFirstJob = "Leaser";
+let myCurrentJob = "SEO";
+
+let job1 = "Lease Agent";
+let job2 = "Organic Search Optimizer";
+
+console.log(myFirstJob);
+*/
+
+true;
+let javascriptIsFun = true;
+let myFirstJob = "Leaser";
+let age = 38;
+let deceasedDate;
+console.log(javascriptIsFun);
+console.log(typeof myFirstJob);
+console.log(typeof age);
+console.log(typeof deceasedDate);
+console.log(typeof true);
