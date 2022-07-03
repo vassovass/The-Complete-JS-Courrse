71eff1a6cc660f69c560dc97f637592a9ec0c83c
Vasso Vassiliades <vaso@vaseo.co.za>
2022-07-03 23:22:51 +0200

    Looked at Operator precedence and basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -34,4 +34,62 @@ console.log(typeof age);
 console.log(typeof deceasedDate);
 console.log(typeof true);
 */
+//Numerical Operators
+/*const currentYear = 2022;
+const ageJonas = currentYear - 1984;
+const ageSarah = currentYear - 1980;
 
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 8);
+// 2 ** 15 means 2 to the power of 15
+
+console.log(ageJonas, ageSarah);
+console.log("is Jonas age greater than sarah", ageJonas > ageSarah);
+console.log("normal age jonas", ageJonas, "sarah", ageSarah);
+console.log("sarah age - 4,is sarah age greater than?", ageJonas < ageSarah - 4);
+console.log("sarah age - 4,is sarah age greater than or =?", ageJonas <= ageSarah - 4);
+console.log("sarah age - 5,is sarah age less than?", ageJonas > ageSarah - 5);
+
+console.log(ageJonas, ageSarah - 4);
+
+let ageThomas = ageSarah + 10.4;
+console.log("thomas age", ageThomas);
+//ageThomas = ceil(ageThomas); //not sure of issue at this stage of why I cant roundup
+//console.log("thomas age", ageThomas.ceil(1));
+
+*/
+//Concatenation
+/*
+let firstName = 'Lisa';
+let lastName = 'Vassiliades';
+console.log(firstName + ' ' + lastName);
+*/
+
+//Assignment Operators
+/*let x = 10 + 5; //15
+x += 12; // x=x+12 = 15+12 = 27
+console.log(x);
+x *= 3; //x=x*3=81
+
+console.log(x);
+
+x++; //x+1=82
+x--; //x-1=81
+x--; //x-1=80
+
+console.log(x);
+
+*/
+
+// Operator Precedence S2E15
+
+const currentYear = 2022;
+const ageJonas = currentYear - 1984;
+const ageSarah = currentYear - 1980;
+
+console.log(currentYear - 1984 > currentYear - 1980);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y)
