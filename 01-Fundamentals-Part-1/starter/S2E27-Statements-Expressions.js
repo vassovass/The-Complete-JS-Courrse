4e9e36203992803bf3b4ab63dce40077bd734202
Vasso Vassiliades <vasso@vaseo.co.za>
2022-07-18 01:33:01 +0200

    S2E27 Statements and Expressions Theory only

---
+++ 01-Fundamentals-Part-1/starter/S2E27-Statements-Expressions.js
@@ -0,0 +1,3 @@
+//This was theory based.
+// Expression = code that produces value . e.g. 3+4, 241, true, true && false &&!true
+// Statements. bigger piece of the code that is executed & duzn't prod value & translate actions
