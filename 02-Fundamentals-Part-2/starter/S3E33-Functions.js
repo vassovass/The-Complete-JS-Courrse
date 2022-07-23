f499494620e56829666326c04966ebcfcb484ce6
Vasso Vassiliades <vaso@vaseo.co.za>
2022-07-23 15:54:26 +0200

    S3E33 Functions Create first function

---
+++ 02-Fundamentals-Part-2/starter/S3E33-Functions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function vegetableProcessor(carrot, butternut) {
+  console.log({ carrot }, { butternut });
+  const soup = `Vegetable soup with ${carrot} carrots & ${butternut} butternuts.`;
+  return soup;
+}
+
+
+// console.log({ carrot }, { butternut }); //carrot not defined due to 'carrot var being declared in function
+
+const vegetableSoup = vegetableProcessor(5, 3);
+console.log(vegetableSoup)
